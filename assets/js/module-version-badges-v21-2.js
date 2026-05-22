730f38ea65ac1469fd06cb891a0826a9da73fb46
Vitaliy <Vitalijimullin07@gmail.com>
2026-05-22 10:46:02 +0300

    Stabilize pool controls V21.7

--- assets/js/module-version-badges-v21-2.js
+++ assets/js/module-version-badges-v21-2.js
@@ -4,8 +4,8 @@
   const ModuleVersions = {
     // Главные функции
     shield: "V13",
-    pool: "V21",
-    rough: "V21",
+    pool: "V21.7",
+    rough: "V21.7",
     materials: "V20",
     works: "V20",
     estimate: "V12",
