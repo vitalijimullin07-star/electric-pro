88bf736a3589d7111b3cf3f444d760ce10481081
Vitaliy <Vitalijimullin07@gmail.com>
2026-05-22 18:39:05 +0300

    Show visible connector lines V21.18

--- assets/js/module-version-badges-v21-2.js
+++ assets/js/module-version-badges-v21-2.js
@@ -4,8 +4,8 @@
   const ModuleVersions = {
     // Главные функции
     shield: "V13",
-    pool: "V21.17",
-    rough: "V21.17",
+    pool: "V21.18",
+    rough: "V21.18",
     materials: "V20",
     works: "V20",
     estimate: "V12",
