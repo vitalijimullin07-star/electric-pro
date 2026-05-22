2d363f1c60c9d1f70aabf19997fbd9bc66a324a7
Vitaliy <Vitalijimullin07@gmail.com>
2026-05-22 11:58:45 +0300

    Add pool keys and logic editor V21.9

--- assets/js/module-version-badges-v21-2.js
+++ assets/js/module-version-badges-v21-2.js
@@ -4,8 +4,8 @@
   const ModuleVersions = {
     // Главные функции
     shield: "V13",
-    pool: "V21.8",
-    rough: "V21.8",
+    pool: "V21.9",
+    rough: "V21.9",
     materials: "V20",
     works: "V20",
     estimate: "V12",
